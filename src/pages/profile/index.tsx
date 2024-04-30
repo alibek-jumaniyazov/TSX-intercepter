5576ab5b919907d544f9f3a66c3908a75790c29d
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-04-30 15:14:00 +0500

    ishladi

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,16 +1,31 @@
+import { useGet } from "../../hooks/use-query";
 import { useDispatch } from "../../hooks";
 import { clearAuth } from "../../redux/slices/authSlice";
 import { useNavigate } from "react-router-dom";
+import { baseUrlPost, prostUrl } from "../../utils/urls";
 
-
+export type TPostResponse = {
+    albumId: number;
+    id: number;
+    title: string;
+    url: string;
+    thumbnailUrl: string;
+};
 export default function ProfilePage() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const { data, isLoading, error } = useGet<TPostResponse[]>(
+        baseUrlPost + prostUrl
+    );
+    console.log(data);
 
     return (
         <div className="ProfilePage">
             <h1>
-                go to <button onClick={() =>(dispatch(clearAuth()) , navigate('/')) }>logout</button>
+                go to{" "}
+                <button onClick={() => (dispatch(clearAuth()), navigate("/"))}>
+                    logout
+                </button>
             </h1>
         </div>
     );
